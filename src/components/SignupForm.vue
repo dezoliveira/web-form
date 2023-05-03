c45555ef7b6070d448d78422157450999b703352
dezoliveira <andresoliveira@protonmail.com>
2023-05-03 18:35:48 -0300

    Select box and Check Box

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,13 +1,29 @@
 <template>
   <form>
+
     <label>Email: </label>
     <input type="email" required v-model="email"/>
 
     <label>Password: </label>
-    <input type="email" required v-model="password"/>
+    <input type="password" required v-model="password"/>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+    
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required/>
+      <label>Accept terms and conditions</label>
+    </div>
+
   </form>
-  <p>Email: {{email}}</p>
-  <p>Email: {{password}}</p>
+
+  <p>Email: {{ email }}</p>
+  <p>Pass: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
 </template>
 
 <script>
@@ -15,7 +31,9 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      role: 'designer',
+      terms: true,
     }
   }
 }
@@ -41,7 +59,7 @@ export default {
       font-weight: bold;
     }
 
-    input {
+    input, select {
       display: block;
       padding: 10px 6px;
       width: 100%;
@@ -49,5 +67,12 @@ export default {
       border: none;
       border-bottom: 1px solid #ddd;
       color: #555;
+    }
+    input[type="checkbox"] {
+      display: inline-block;
+      width: 16px;
+      margin: 0 10px 0 0;
+      position: relative;
+      top: 2px;
     }
 </style>
